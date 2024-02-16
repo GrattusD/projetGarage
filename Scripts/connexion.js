adce95c48609eac50c4473b03ef32f7c05a233ec
GrattusD <edouardmeyer94@gmail.com>
2024-02-16 18:56:01 +0100

    ajout dossier css et script, ajout de pages html maj index et style

---
+++ Scripts/connexion.js
@@ -0,0 +1,5 @@
+/*recupérer les données du formulaire de connexion*/
+/*comparer les données avec la base de données*/
+/*si pas ok ==> erreur*/
+/*si ok ==>si admin renvoie vers page sécurisée admin*/
+/*si ok ==>si employé renvoie vers page sécurisée employé*/
