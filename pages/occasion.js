32d0f3b04b66332851d6fe6cf334c1057d547861
GrattusD <edouardmeyer94@gmail.com>
2024-04-09 16:16:58 +0200

    mise a jour base de données

---
+++ pages/occasion.js
@@ -0,0 +1,7 @@
+const element = React.createElement('h1', {}, 'Véhicules disponibles')
+
+const container = document.getElementById('root')
+
+const root = ReactDOM.createRoot(container)
+
+root.render(element)
